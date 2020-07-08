26c6f32da01427129ba01f7b1b0991ccb5e4959d
Nail Karimov <d.r.freeman1987@mail.ru>
2020-07-08 21:35:23 +0300

    3.3. Разбор первой программы. [#320580]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nail Karimov");
+        System.out.println("24.07.1987");
+
+    }
+}
