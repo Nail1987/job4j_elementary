0a6dd66c817781a17fa8d5e017403ca495b647e4
Nail Karimov <d.r.freeman1987@mail.ru>
2020-07-12 16:45:11 +0300

    5. Конвертер валюты [#320559]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,19 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static int rubleToEuro( int value ){
+        int rls = value/70;
+        return rls;
+    }
+    public static int rubleToDollar( int value ){
+        int rls = value/60;
+        return rls;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(70);
+        int dollar = Converter.rubleToDollar(60);
+        System.out.println("70 rubles are " + euro  + " euro");
+        System.out.println("60 rubles are " + dollar  + " dollar");
+    }
+}
