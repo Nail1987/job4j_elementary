e7f8bbbe41999c03a103688efd3b503b5d73c1c7
Nail Karimov <d.r.freeman1987@mail.ru>
2020-07-15 21:07:32 +0300

    9. Площадь треугольника. [#320566] Исправление задачи 2

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -1,15 +1,15 @@
 package ru.job4j.condition;
 
 public class TrgArea {
-    public static double area (double a, double b, double c){
-        double rls = (a + b + c)/2;
-        double four = Math.sqrt(9);
-        return  four;
-
+    public static double area(double a, double b, double c) {
+        double halfPerimetr = (a + b + c) / 2;
+        double value = halfPerimetr * (halfPerimetr - a) * (halfPerimetr - b) * (halfPerimetr - c);
+        double sqrt = Math.sqrt(value);
+        return sqrt;
     }
 
     public static void main(String[] args) {
-        double result = TrgArea.area(2,2,2);
+        double result = TrgArea.area(2, 2, 2);
         System.out.println("area (2, 2, 2) = " + result);
     }
 }
