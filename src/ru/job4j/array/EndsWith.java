72bd599608766338aeb44c6bd6d2459a21150e98
Nail Karimov <d.r.freeman1987@mail.ru>
2020-10-18 17:50:54 +0300

    6.4.2. Слово заканчивается на ... [#320537] с исправлением

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -5,7 +5,7 @@ public class EndsWith {
 
         boolean result = true;
         for (int i = 0; i < post.length; i++) {
-            if (post[post.length - 1] != word[word.length - 1]) {
+            if (post[post.length - i - 1] != word[word.length - i - 1]) {
                 result = false;
                 break;
             }
